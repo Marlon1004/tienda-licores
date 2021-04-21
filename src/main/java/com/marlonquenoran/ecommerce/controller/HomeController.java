0f5c53d8b9907b96cc3543373a64df85b9358006
Your Name <you@example.com>
2021-04-20 23:47:06 -0500

    Mostrar los datos del producto en la vista productohome

--- src/main/java/com/marlonquenoran/ecommerce/controller/HomeController.java
+++ src/main/java/com/marlonquenoran/ecommerce/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.marlonquenoran.ecommerce.controller;
 
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.marlonquenoran.ecommerce.model.Producto;
 import com.marlonquenoran.ecommerce.service.ProductoService;
 
 @Controller
@@ -29,9 +32,15 @@ public class HomeController {
 	
 	
 	@GetMapping("productohome/{id}")
-	public String productoHome(@PathVariable Integer id) {
+	public String productoHome(@PathVariable Integer id, Model model) {
 		
 		log.info("Id enviado como parametro {}", id);
+		
+		Producto producto= new Producto();
+		Optional<Producto> productoOptional=productoService.get(id);
+		producto=productoOptional.get();
+		
+		model.addAttribute("producto", producto);
 		
 		return "usuario/productohome";
 	}
