f35416bef8abef843d36a81eee5ea5ae19f1d86f
Your Name <you@example.com>
2021-04-23 00:22:17 -0500

    funcionalidad para buscar un producto

--- src/main/java/com/marlonquenoran/ecommerce/controller/HomeController.java
+++ src/main/java/com/marlonquenoran/ecommerce/controller/HomeController.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -198,6 +200,19 @@ public class HomeController {
 		detalle.clear();
 		
 		return "redirect:/";
+	}
+	
+	
+	
+	@PostMapping("/search")
+	public String serachProducto(@RequestParam String nombre, Model model) {
+		
+		log.info("Nombre del producto: {}", nombre);
+		
+		List<Producto> productos=productoService.findAll().stream().filter(p -> p.getNombre().contains(nombre)).collect(Collectors.toList());
+		model.addAttribute("productos", productos);
+		
+		return "usuario/home";
 	}
 	
 	
