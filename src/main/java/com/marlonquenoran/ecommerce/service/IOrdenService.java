ed4c2a773b7f0c45a0e6bf654d4ad9f3a3857891
Your Name <you@example.com>
2021-04-22 23:04:47 -0500

    metodo para generar el numero de la orden

--- src/main/java/com/marlonquenoran/ecommerce/service/IOrdenService.java
+++ src/main/java/com/marlonquenoran/ecommerce/service/IOrdenService.java
@@ -1,8 +1,12 @@
 package com.marlonquenoran.ecommerce.service;
 
+import java.util.List;
+
 import com.marlonquenoran.ecommerce.model.Orden;
 
 public interface IOrdenService {
+	
+	List<Orden> findAll();
 	
 	Orden save(Orden orden);
 
